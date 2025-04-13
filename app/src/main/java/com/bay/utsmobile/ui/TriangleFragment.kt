8852c128a7bf9af9b300ba1dab9811f0f5ea7f68
Farel <farelbayhaqi2544@gmail.com>
2025-04-13 23:20:57 +0700

    buttons, triangle logic

--- app/src/main/java/com/bay/utsmobile/ui/TriangleFragment.kt
+++ app/src/main/java/com/bay/utsmobile/ui/TriangleFragment.kt
@@ -2,59 +2,38 @@ package com.bay.utsmobile.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.bay.utsmobile.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TriangleFragment : Fragment(R.layout.fragment_triangle) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TriangleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TriangleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        val tinggiEdt = view.findViewById<EditText>(R.id.tinggiEdt)
+        val alasEdt = view.findViewById<EditText>(R.id.alasEdt)
+        val hasilBtn = view.findViewById<Button>(R.id.hasilBtn)
+        val hasilLuasTv = view.findViewById<TextView>(R.id.hasilLuasTv)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+        hasilBtn.setOnClickListener {
+            val tinggiStr = tinggiEdt.text.toString()
+            val alasStr = alasEdt.text.toString()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_triangle, container, false)
-    }
+            if (tinggiStr.isNotEmpty() && alasStr.isNotEmpty()) {
+                val tinggi = tinggiStr.toDoubleOrNull()
+                val alas = alasStr.toDoubleOrNull()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TriangleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TriangleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                if (tinggi != null && alas != null) {
+                    val luas = 0.5 * alas * tinggi
+                    hasilLuasTv.text = "Luas segitiga: %.2f".format(luas)
+                } else {
+                    hasilLuasTv.text = "Input harus berupa angka"
                 }
+            } else {
+                hasilLuasTv.text = "Tinggi dan alas tidak boleh kosong"
             }
+        }
     }
 }
