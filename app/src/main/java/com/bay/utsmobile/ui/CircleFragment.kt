4aad31797a2e2ce2c15dce615d7fe79642bcc8b9
Farel <farelbayhaqi2544@gmail.com>
2025-04-13 23:43:10 +0700

    circle logic

--- app/src/main/java/com/bay/utsmobile/ui/CircleFragment.kt
+++ app/src/main/java/com/bay/utsmobile/ui/CircleFragment.kt
@@ -5,56 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.bay.utsmobile.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CircleFragment : Fragment(R.layout.fragment_circle) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CircleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CircleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        val jariJariEdt = view.findViewById<EditText>(R.id.jarijariEdt)
+        val hasilBtn = view.findViewById<Button>(R.id.hasilBtn)
+        val hasilLuasTv = view.findViewById<TextView>(R.id.hasilLuasTv)
+        val hasilKelilingTv = view.findViewById<TextView>(R.id.hasilKelilingTv)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+        hasilBtn.setOnClickListener {
+            val jariStr = jariJariEdt.text.toString()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_circle, container, false)
-    }
+            if (jariStr.isNotEmpty())  {
+                val jariJari = jariStr.toDoubleOrNull()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CircleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CircleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                if (jariJari != null) {
+                    val luas = 3.14159 * jariJari * jariJari
+                    hasilLuasTv.text = "Luas lingkaran : %.2f".format(luas)
+
+                    val keliling = 3.14159 * 2 * jariJari
+                    hasilKelilingTv.text = "Keliling lingkaran : %.2f".format(keliling)
+                } else {
+                    hasilLuasTv.text = "Input harus berupa angka"
                 }
+            } else {
+                hasilLuasTv.text = "Tinggi dan alas tidak boleh kosong"
             }
+        }
     }
 }
